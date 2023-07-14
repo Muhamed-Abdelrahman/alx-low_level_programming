b0274f39e2c41579da5672a4de250226a4214851
mohamed <mohamed@gmail.com>
2023-07-14 10:10:28 -0700

    sdfds

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 2-print_alphabet.c
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char i = 97, a = 65;
+
+	for (; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	for (; a <= 90; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+}
