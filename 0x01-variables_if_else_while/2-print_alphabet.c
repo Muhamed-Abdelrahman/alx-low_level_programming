9cf251c4fd8db8ebecc4d92d81a2112f21ecd541
mohamed <mohamed@gmail.com>
2023-07-14 09:45:14 -0700

    asad

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 2-print_alphabet.c
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char i = 97;
+
+	for (; i <= 122; i++)
+	{
+		putchar(i);
+	}
+
+	return (0);
+}
