ca13da5b221ad9253d6f21081b5dc49c2732c6c2
mohamed <mohamed@gmail.com>
2023-07-20 15:13:43 -0700

    dfsa

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: input for digit
+ *
+ * @b: input for digit
+ *
+ * Return: multiplies two integers
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
