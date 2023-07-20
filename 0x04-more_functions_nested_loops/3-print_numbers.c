a26e4cdbf607ac7ad1d9119ec9c7afc3ac555b52
mohamed <mohamed@gmail.com>
2023-07-20 15:20:49 -0700

    dfd

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - multiplies two integers
+*/
+
+void print_numbers(void)
+{
+	int i = 0
+
+	for (i ; i <= 9 ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
