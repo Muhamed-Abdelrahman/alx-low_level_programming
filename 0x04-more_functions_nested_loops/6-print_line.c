1ecb7077c92721caafa09e5daf2baecc88f20b42
mohamed <mohamed@gmail.com>
2023-07-20 17:05:06 -0700

    rfer

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - multiplies two integers
+ *              only using _putchar twice
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
