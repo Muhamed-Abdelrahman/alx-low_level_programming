d9530284af58973d22012eb434ef6471484749be
mohamed <mohamed@gmail.com>
2023-07-24 08:37:33 -0700

    gregtre

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - check if c is upper
+ *
+ * @s: is var
+ *
+ * Return: size
+*/
+
+int _strlen(char *s)
+{
+	int cont = 0;
+
+	for (cont; *s != '\0'; s++)
+		cont++;
+	return (cont);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - check if c is upper
+ *
+ * @str: is var
+ *
+ * Return: print follwed by line
+*/
+
+void _puts(char *str)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s + 0);
+		s++;
+	}
+	_putchar('\n');
+}
