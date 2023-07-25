6b20b3aafef1871b3294291ed1a18626119cb0c2
mohamed <mohamed@gmail.com>
2023-07-24 17:33:35 -0700

    fwewe

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - check if c is upper
+ *
+ * @s: is var
+ *
+ * Return: print follwed by line
+*/
+
+void rev_string(char *s)
+{
+	int i = 0, l;
+	char temp;
+
+	while (s[i])
+		i++;
+for (l = 0,  l < i / 2, l++)
+{
+	temp = s[l];
+	s[l] = s[i - 1 - l];
+	s[i - 1 - l] = temp;
+}
+}
