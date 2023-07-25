f19034a414d108d3e5138da0065385b8a8a8ad29
mohamed <mohamed@gmail.com>
2023-07-24 17:52:37 -0700

    fwswe

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - check if c is upper
+ *
+ * @str: is var
+ *
+ * Return: print follwed by line
+*/
+
+void puts_half(char *str)
+{
+	int i = 0, a;
+
+	while (str[i])
+		i++;
+	for (a = i / 2; str != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
