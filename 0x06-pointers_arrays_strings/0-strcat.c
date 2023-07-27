b9d11f15a6ddc6545c810417bba4177495d6cb0a
mohamed <mohamed@gmail.com>
2023-07-26 23:05:33 -0700

    vssd

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,11 +12,12 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int a = 0, b = 0;
+	int a, b;
 
+	a = 0;
 	while (dest[a])
 		a++;
-	for (b; src[b]; b++)
+	for (b = 0; src[b]; b++)
 	{
 		dest[a++] = src[b];
 	}
