732ecd06dd7df3083309b1fba0f5ebfc82f8f649
mohamed <mohamed@gmail.com>
2023-07-26 21:16:19 -0700

    gtrg

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcat - check if c is upper
+ *
+ * @dest: is var
+ *
+ * @src: is var
+ *
+ * Return: print follwed by line
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (dest);
+}
