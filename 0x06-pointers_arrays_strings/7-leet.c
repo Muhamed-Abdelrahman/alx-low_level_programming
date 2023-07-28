176b1225da7487c0a91e7ef186a76695568de7df
mohamed <mohamed@gmail.com>
2023-07-27 18:24:58 -0700

    fe

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - check if c is upper
+ *
+ * @c: is var
+ *
+ * Return: print follwed by line
+*/
+
+char *leet(char *c)
+{
+	char *cp = c;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int val[] = {4, 3, 0, 7, 1};
+
+	while (*c)
+	{
+		for (i = 0; i < sizeof(k) / sizeof(char); i++)
+		{
+			if (*c == key[i] || *c == key[i] + 32)
+			{
+				*c = val[i] + 48;
+			}
+		}
+		c++;
+	}
+	return (cp);
+}
