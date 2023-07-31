7a63106ad1dee30f53a1771a1485644772ca5aed
mohamed <mohamed@gmail.com>
2023-07-31 15:24:25 -0700

    erg

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strspn - check if c is upper
+ *
+ * @s: is var
+ *
+ * @accept: is var
+ *
+ * Return: s
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int cont;
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				cont++;
+				break;
+			}
+		}
+	}
+	if (accept[j] == '\0')
+	{
+		break;
+	}
+	return (cont);
+}
