41658e8fdccef3d7c0a5d05209f8c4d7886c1f9b
mohamed <mohamed@gmail.com>
2023-08-02 01:40:59 -0700

    fwf

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - check if c is upper
+ *
+ * @s: is var
+ *
+ * Return: print follwed by line
+*/
+
+void _puts_recursion(char *s)
+{
+	if (s == '\0')
+	{
+		_putchar('\n');
+		return (0);
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
