9ebd3d7e944f98dfc699069d8bd32bc66c800034
mohamed <mohamed@gmail.com>
2023-08-02 03:14:01 -0700

    gfd

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - check if c is upper
+ *
+ * @n: is var
+ *
+ * Return: print follwed by line
+*/
+
+int square(int n, int val);
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
+
+#include "main.h"
+
+/**
+ * square - check if c is upper
+ *
+ * @n: is var
+ *
+ * @val: is var
+ *
+ * Return: print follwed by line
+*/
+
+int square(int n, int val)
+{
+	if (val * val == n)
+		return (val);
+	else if (val * val < n)
+		return (square(n, val + 1));
+	else
+		return (-1);
+}
