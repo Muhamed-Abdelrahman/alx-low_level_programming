f7ff2fbcf9e9e3398ed2e89b25ce812953184cd4
mohamed <mohamed@gmail.com>
2023-08-04 14:13:33 -0700

    fe

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+
+/**
+ * main - check if c is upper
+ *
+ * @argc: is var
+ *
+ * @argv: is var
+ *
+ * Return: print follwed by line
+*/
+
+int main(int argc, char const *argc[])
+{
+	(void)argc;
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
