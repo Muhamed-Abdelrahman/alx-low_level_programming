fb09ae33f927c47f6c0cf3dbdb57566f4a785d1e
mohamed <mohamed@gmail.com>
2023-08-07 21:42:13 -0700

    geg

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,11 +12,11 @@
 
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
-		return (0);
 	char *ptr;
 	unsigned int i;
 
+	if (size == 0)
+		return (0);
 	ptr = malloc(sizeof(char) * size);
 	if (ptr == 0)
 		return (0);
