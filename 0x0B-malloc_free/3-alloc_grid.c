9bd8371daccd617cdd5c8cbd64ac1c3440e6f96a
mohamed <mohamed@gmail.com>
2023-08-09 00:16:41 -0700

    ger

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,6 +20,13 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = calloc(width, sizeof(int **));
+		if (grid[i] == NULL)
+		{
+			for (int j = 0; j < i; j++)
+			free(grid[j]);
+		free(grid);
+		return (NULL);
+		}
 	}
 	return (grid);
 
