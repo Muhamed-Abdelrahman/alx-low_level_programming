e7402ac65b1731c6170d67c27c4de177b27bc1ae
mohamed <mohamed@gmail.com>
2023-08-14 02:38:39 -0700

    tete

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: a dog's name
+ * @age: a a dog's age
+ * @owner: a dog's owner
+ *
+ * Description: struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
