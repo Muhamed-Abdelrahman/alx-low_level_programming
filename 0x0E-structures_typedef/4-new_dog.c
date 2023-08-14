5a1f34cf10f5d89fd2a7eaf566225d4277e82235
mohamed <mohamed@gmail.com>
2023-08-14 04:55:04 -0700

    terw

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -53,7 +53,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->owner = malloc(s2 + 1);
 	if ((*dog).owner == NULL)
 	{
-		free(dog->name)
+		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
