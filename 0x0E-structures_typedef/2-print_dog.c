2047318846aacddd2e3158183da470d2c25091b4
mohamed <mohamed@gmail.com>
2023-08-14 03:09:37 -0700

    hrryh

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * print_dog - check if c is upper
+ *
+ * @d: is var
+ *
+ * Return: print follwed by line
+*/
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("age: %f\n", d->age);
+		printf("owner: %s\n", d->age ? d->owner : "(nil)");
+	}
+}
