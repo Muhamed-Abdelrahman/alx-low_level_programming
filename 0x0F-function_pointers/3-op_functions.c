54b5ae7681b93750d5565504ad472cae65ca4387
mohamed <mohamed@gmail.com>
2023-08-16 09:14:27 -0700

    ger

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+#include "3-calc.h"
+
+/**
+ * op_add - check if c is upper
+ *
+ * @a: is var
+ * @b: is var
+ *
+ * Return: print follwed by line
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - check if c is upper
+ *
+ * @a: is var
+ * @b: is var
+ *
+ * Return: print follwed by line
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - check if c is upper
+ *
+ * @a: is var
+ * @b: is var
+ *
+ * Return: print follwed by line
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - check if c is upper
+ *
+ * @a: is var
+ * @b: is var
+ *
+ * Return: print follwed by line
+*/
+int op_div(int a, int b)
+{
+
+	return (a / b);
+}
+/**
+ * op_mod - check if c is upper
+ *
+ * @a: is var
+ * @b: is var
+ *
+ * Return: print follwed by line
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
