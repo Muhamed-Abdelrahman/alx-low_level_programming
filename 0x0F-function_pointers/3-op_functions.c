775b5574472cd09fe338af60a055d357d1a5eec5
mohamed <mohamed@gmail.com>
2023-08-16 11:10:13 -0700

    ter

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,12 +1,14 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 
 /**
- * op_add - check if c is upper
- *
- * @a: is var
- * @b: is var
- *
- * Return: print follwed by line
+* op_add - ...
+* @a: ...
+* @b: ...
+*
+* Return: ...
 */
 int op_add(int a, int b)
 {
@@ -14,51 +16,67 @@ int op_add(int a, int b)
 }
 
 /**
- * op_sub - check if c is upper
- *
- * @a: is var
- * @b: is var
- *
- * Return: print follwed by line
+* op_sub - ...
+* @a: ...
+* @b: ...
+*
+* Return: ...
 */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
+
 /**
- * op_mul - check if c is upper
- *
- * @a: is var
- * @b: is var
- *
- * Return: print follwed by line
+* op_mul - ...
+* @a: ...
+* @b: ...
+*
+* Return: ...
 */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
+
 /**
- * op_div - check if c is upper
- *
- * @a: is var
- * @b: is var
- *
- * Return: print follwed by line
+* op_div - ...
+* @a: ...
+* @b: ...
+*
+* Return: ...
 */
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 
 	return (a / b);
 }
+
+
 /**
- * op_mod - check if c is upper
- *
- * @a: is var
- * @b: is var
- *
- * Return: print follwed by line
+* op_mod - ...
+* @a: ...
+* @b: ...
+*
+* Return: ...
 */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+
 	return (a % b);
 }
+
