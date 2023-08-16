a210e2b2d8a21e8f329120ee0840bf3b3acad422
mohamed <mohamed@gmail.com>
2023-08-16 06:52:00 -0700

    ger

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - check if c is upper
+ *
+ * @array: is var
+ * @size: is var
+ * @action: is var
+ *
+ * Return: print follwed by line
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (size && action)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
