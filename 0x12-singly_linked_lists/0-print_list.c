6962a2b5c2fffbc76da9377e01ad59d05790d0a1
mohamed_abdo <mohamed_abdo.com>
2023-08-24 08:38:50 -0700

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,40 @@
+#include "list.h"
+
+/**
+ * _strln - count the char
+ *
+ * @str: is var
+ *
+ * Return: number
+*/
+int _strln(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	return (i);
+}
+
+/**
+ * print_list - print all elelment
+ *
+ * @h: is var
+ *
+ * Return: 0
+*/
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strln(h->str), h->str ? h->str : (nil));
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
