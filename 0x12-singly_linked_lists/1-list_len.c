e86db8f139489bfed41ddad2d4ae29e39f54df62
mohamed_abdo <mohamed_abdo.com>
2023-08-24 09:17:52 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - count the char
+ *
+ * @h: is var
+ *
+ * Return: number
+*/
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
